0ffcfc60e68b60f6473c4b209e4d958ad8524f7d
unknown <katlego@callitservices.co.za>
2021-03-24 11:31:39 +0200

    Fix Passport Redirect-FrontEnd

--- routes/profile.js
+++
@@ -1,8 +0,0 @@
-const router = require('express').Router();
-const isAuth = require('../mw/isAuth');
-
-router.get('/', isAuth, (req, res) => {
-  res.send('valid user');
-});
-
-module.exports = router;
