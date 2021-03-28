3cb6c68d9c89a298741718e8f6993c5db93da21a
unknown <katlego@callitservices.co.za>
2021-03-28 09:19:39 +0200

    console user on callback

--- routes/auth.js
+++ routes/auth.js
@@ -81,10 +81,11 @@ router.get('/facebook', passport.authenticate('facebook', { scope: 'email' }));
 
 router.get(
   '/facebook/callback',
-  passport.authenticate('facebook', {
-    successRedirect: '/profile',
-    failureRedirect: '/',
-  })
+  passport.authenticate('facebook'),
+  (req, res) => {
+    console.log('Facebook Callback: ', req.user);
+    res.redirect('/profile');
+  }
 );
 
 //Google Login Route//
@@ -94,6 +95,7 @@ router.get(
 );
 
 router.get('/google/callback', passport.authenticate('google'), (req, res) => {
+  console.log('Google Callback: ', req.user);
   res.redirect('/profile');
 });
 //Log User Out//
